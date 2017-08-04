fce99ad58a882122abb7c197145972e2932ae3b6
RhymeLyet <rhymeinparentheses@gmail.com>
2017-08-04 16:58:49 -0700

    Used %
    Used %

---
+++ src/JustenBiber.java
@@ -0,0 +1,14 @@
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class JustenBiber {
+public static void main(String[] args) {
+	for (int i = 0; i < 3; i++) {
+		System.out.println("baby");
+	}
+		System.out.println("oh");
+		for (int j = 0; j < 3; j++) {
+		System.out.println("baby");
+		}
+		System.out.println("no");
+}
+}
---
+++ src/WarmUp.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class WarmUp {
+public static void main(String[] args) {
+	for (int i = 0; i < 100; i++) {
+		
+	
+	if (i%2==0) {
+		System.out.println("x");
+	}
+	if (i%2==1) {
+		System.out.println("o");
+	}
+	}
+}
+}
